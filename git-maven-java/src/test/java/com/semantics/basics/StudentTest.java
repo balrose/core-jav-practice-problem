1e19a4fd914f7cd9153a101a67f01b5801794024
Rose <roze.mahi@gmail.com>
2019-08-07 18:45:23 -0400

    testing a new project

---
+++ git-maven-java/src/test/java/com/semantics/basics/StudentTest.java
@@ -0,0 +1,14 @@
+package com.semantics.basics;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentTest {
+
+	@Test
+	public void testEnroll() {
+		fail("Not yet implemented");
+	}
+
+}
