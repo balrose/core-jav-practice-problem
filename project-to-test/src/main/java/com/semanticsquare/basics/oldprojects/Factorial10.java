0ee2ff9f0b450ae1f9e668257ba8edbf4a8b2b46
Rose <roze.mahi@gmail.com>
2019-08-17 19:06:11 -0400

    updated

--- project-to-test/src/main/java/com/semanticsquare/basics/oldprojects/Factorial10.java
+++ project-to-test/src/main/java/com/semanticsquare/basics/oldprojects/Factorial10.java
@@ -7,9 +7,9 @@ public class Factorial10 {
         public static double factorial(double n)
         {
           
-        	//TODO: 0! should return 1, if you please fix the issue, thanks
-            if (n <= 1) {
-                return n;
+        	
+            if (n <=0) {
+                return n+1;
             }
             for(int i=0;i<=n;i--);
             return factorial(n - 1)*n;
